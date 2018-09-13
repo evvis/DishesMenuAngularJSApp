d7c238f5f92fe8c0d7d046053100d14c7b52d04a
Evgeniy Viskunov <evgeniy.viskunov@live.com>
2018-09-14 02:05:06 +0700

    Adding sorting

--- app/controllers.js
+++ app/controllers.js
@@ -1,8 +1,27 @@
 'use strict';
 
-angular.module('dishesApp', [])
+angular.module('dishesApp', ['ui.bootstrap'])
     .controller('DishesList', ['$scope', '$http', function($scope, $http) {
-        $http.get('/menu.json').success(function (data, status, headers, config) {
+        $http.get('/menu.json').success(function (data) {
             $scope.dishes = data;
         });
+
+        $scope.sortField = undefined;
+        $scope.reverse = false;
+
+        $scope.sort = function (fieldName) {
+            if ($scope.sortField === fieldName){
+                $scope.reverse = !$scope.reverse;
+            } else {
+                $scope .sortField = fieldName;
+                $scope.reverse = false;
+            }
+        };
+        $scope.isSortUp = function (fieldName) {
+            return $scope.sortField === fieldName && !$scope.reverse;
+        };
+        $scope.isSortDown = function (fieldName) {
+            return $scope.sortField === fieldName && $scope.reverse;
+        };
     }]);
+
