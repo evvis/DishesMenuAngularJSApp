15182963af9fdfda9de64623522815737e9d3fa1
Evgeniy Viskunov <evgeniy.viskunov@live.com>
2018-09-14 19:36:37 +0700

    Add modal dialog window

--- app/controllers.js
+++ app/controllers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('dishesApp', ['ui.bootstrap'])
-    .controller('DishesList', ['$scope', '$http', function($scope, $http) {
+    .controller('DishesList', ['$scope', '$http', '$uibModal', '$log', function($scope, $http, $uibModal, $log) {
         $http.get('/menu.json').success(function (data) {
             $scope.dishes = data;
         });
@@ -23,5 +23,24 @@ angular.module('dishesApp', ['ui.bootstrap'])
         $scope.isSortDown = function (fieldName) {
             return $scope.sortField === fieldName && $scope.reverse;
         };
-    }]);
 
+        $scope.modalUpdate = function (dish) {
+            var modalInstance = $uibModal.open({
+                templateUrl: 'itemIngridients.html',
+                controller: function ($scope, dish) {
+                    $scope.dish = dish;
+                },
+                resolve: {
+                    dish: function () {
+                        return dish;
+                    }
+                }
+            });
+            modalInstance.result.then(function (dish) {
+                $scope.selected = dish;
+                console.log(dish);
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+    }]);
